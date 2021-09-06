51b58d0a142e34f7bda555b0fae6a86f7c071550
LewisCM14 <Lewiscmartin@live.co.uk>
2021-09-06 14:11:53 +0000

    add notes to js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Wait for the dom to finish loading before running the game
+ * Get the button elements and add event listeners to them
+ * Start countdown once a button has been clicked
+ */
+ 
+document.addEventListener("DOMContentLoaded", function()
+
+/**
+ * The main game function
+ * Makes computer pick a random attack
+ * Compares playerChoice to comChoice 
+ * Displays result
+ */
+
+function compare()
+
+/**
+ * Adds point to score if win
+ * Alerts when countdown end
+ * Comapres final score to high score, updates highscore if required
+ */
+
